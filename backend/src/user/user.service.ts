d86c1ea99e199b855c24bdd909799e558da1c45f
Abdallah Taha <abdallahkhoder59@gmail.com>
2026-07-23 21:10:35 +0300

    make user entity with controller & services (CRUD)

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { paginate, PaginateConfig, Paginated, PaginateQuery } from 'nestjs-paginate';
+import { UpdateUser } from 'src/dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -15,5 +17,43 @@ export class UserService {
     const hashed = await bcrypt.hash(user.password, 10);
     const newUser = this.userRepository.create({ ...user, password: hashed });
     return await this.userRepository.save(newUser);
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    return user;
+  }
+
+  async findAll(query: PaginateQuery): Promise<Paginated<User | null>> {
+    const config: PaginateConfig<User> = {
+      sortableColumns: ['id', 'fullName', 'email', 'createdAt'],
+      searchableColumns: ['fullName', 'email'],
+      defaultSortBy: [['createdAt', 'ASC']],
+      withDeleted: true,
+      maxLimit: 30,
+    };
+    return paginate(query, this.userRepository, config);
+  }
+
+  async update(id: number, user: UpdateUser): Promise<User | null> {
+    await this.userRepository.update(id, user);
+    const updatedUser = await this.findOne(id);
+    return updatedUser;
+  }
+
+  async deleteUser(id: number): Promise<boolean> {
+    const status = await this.userRepository.delete(id);
+    const affected = status.affected ?? 0;
+    return affected > 0;
+  }
+
+  async softDelete(id: number): Promise<boolean> {
+    const status = await this.userRepository.softDelete(id);
+    const affected = status.affected ?? 0;
+    return affected > 0;
   }
 }
